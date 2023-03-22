b0284404ba42c7e0731a4b079ee71df01ab8e179
Jaichyi Chen <jaichyichen@gmail.com>
2023-03-22 17:02:29 -0400

    added review backend functionalities

---
+++ backend/SpringBootLibrary/src/main/java/com/luv2read/springbootlibrary/controller/ReviewController.java
@@ -0,0 +1,42 @@
+package com.luv2read.springbootlibrary.controller;
+
+import com.luv2read.springbootlibrary.requestmodels.ReviewRequest;
+import com.luv2read.springbootlibrary.service.ReviewService;
+import com.luv2read.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService){
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/secure")
+    public void postReview(@RequestHeader(value="Authorization")String token, @RequestBody ReviewRequest reviewRequest) throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if(userEmail == null){
+            throw new Exception("user email is missing");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+
+    @GetMapping("/secure/user/book")
+    public boolean reviewBookByUser(@RequestHeader(value="Authorization")String token, @RequestParam Long bookId) throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+
+        if(userEmail == null){
+            throw new Exception("user email is missing");
+        }
+
+        return reviewService.userReviewListed(userEmail, bookId);
+    }
+
+}
